95d0bacdb762dda6f94fdaf7b47940570470278b
danl411412 <daan.b.lee@gmail.com>
2018-01-25 02:55:17 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,5 +25,7 @@ def adding
 
   #your code here
   #remember to return the shipping_manifest hash
-
+  shipping_manifest["muskets"] = 3
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest
 end
